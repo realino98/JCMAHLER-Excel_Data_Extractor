18978fa10139953a048553f2420bed696b9a1ccb
Bernard <bernardinus.r.w.k.j@polytron.co.id>
2022-07-06 10:24:16 +0700

    BR : Added some Indicators

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ df = pd.read_excel('Files/Referrals.xlsx', sheet_name=[FIRST_SHEET_NAME, SECOND_
 df1 = df[FIRST_SHEET_NAME]
 df2 = df[SECOND_SHEET_NAME]
 
+print("Getting all the company Names...")
 companies_1 = []
 # print(len(df1["To Company"]))
 for i in range(len(df1["To Company"])):
@@ -35,9 +36,9 @@ for i in range(len(df2["From Company"])):
         pass
 
 # print(companies_2, len(companies_2))
-
-for i in range(3):
-    print(i)
+print("Generating Files...")
+for i in range(len(companies_1)):
+    # print(i)
     path = OUTPUT+companies_1[i]+".xlsx"
     new_df_1 = df1.loc[df1["To Company"].isin([companies_1[i], companies_1[i]+" Total"])]
     print("Creating Sheet 1", companies_1[i]+".xlsx")
@@ -47,8 +48,8 @@ for i in range(3):
     writer.close()
     # print(new_df_1)
 
-for i in range(3):
-    print(i)
+for i in range(len(companies_2)):
+    # print(i)
     path = OUTPUT+companies_2[i]+".xlsx"
     new_df_2 = df2.loc[df2["From Company"].isin([companies_2[i], companies_2[i]+" Total"])]
     print("Creating Sheet 2", companies_2[i]+".xlsx")
@@ -61,6 +62,6 @@ for i in range(3):
     new_df_2.to_excel(writer, sheet_name = SECOND_SHEET_NAME)
     writer.save()
     writer.close()
-    print(new_df_2)
+    # print(new_df_2)
 
 print("Excel File Generated to Output/..")
